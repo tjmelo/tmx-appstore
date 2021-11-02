7751319341fb3e6fea7ffe8d163c78619be0b6b9
Thiago Melo <thiago.tmwd@gmail.com>
2021-11-02 18:08:57 -0300

    Solving issue component shoes

--- src/Components/shoes/Shoes.tsx
+++ src/Components/shoes/Shoes.tsx
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from "react";
 import { getShoes } from "../../service/api";
 import { Cards } from "../cards/Cards";
 
+interface ShoesProps {}
+
 export const Shoes = () => {
   const [products, setProducts] = useState([]);
-  useEffect(async () => {
-    const shoes = await getShoes;
-    setProducts(shoes.data);
+  useEffect(() => {
+    (async () => {
+      const shoes = await getShoes;
+      setProducts(shoes.data);
+    })();
   }, [products]);
 
   return (
     <>
-      <h1 className="title">Produtos</h1>
-      <h2 className="subTitle">Promoções em destaque</h2>
+      <h1 className="title">Products</h1>
+      <h2 className="subTitle">Featured Promotions</h2>
       <section
         className="containerPromotions"
         data-testid="shoes-promotions"
       >
-        {products.map((el, idx) => (
+        {products.map((el: any, idx: number) => (
           <Cards
             key={idx}
             id={el.id}
