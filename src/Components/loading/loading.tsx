055822ca0ec85b66013cefae17d4b909d138fc1d
Thiago Melo <thiago.tmwd@gmail.com>
2024-05-17 18:16:12 -0300

    feat: improving component loading

--- src/Components/loading/loading.tsx
+++ src/Components/loading/loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import loading from "./loading.module.scss";
 
-export const Loading = () => {
+export const Loading = (): JSX.Element => {
   return (
     <div className={loading.loading}>
       <img src="/loading.gif" />
