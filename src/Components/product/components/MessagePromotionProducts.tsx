f8b7275992b1caccea3199a30354d56b2c73cd99
Thiago Melo <thiago.tmwd@gmail.com>
2024-06-05 16:40:25 -0300

    feat: improving message promotion component

--- src/Components/product/components/MessagePromotionProducts.tsx
+++ src/Components/product/components/MessagePromotionProducts.tsx
@@ -1,42 +1,41 @@
 import React from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons/faExclamationCircle";
 
 import style from "../product.module.scss";
 
-declare interface MessagePromotionProductsProps {
-  reference: any;
-  qtde: number;
+type MessagePromotionProductsProps = {
+    reference: {
+        promotion: string,
+        promo: string,
+    };
+    qtde: number;
 }
 
-const MessagePromotionProducts: React.VFC<MessagePromotionProductsProps> =
-  ({ reference, qtde }) => {
-    let renderMessage = null;
+const MessagePromotionProducts: React.FC<MessagePromotionProductsProps> = ({
+    reference,
+    qtde,
+}) => {
+    const toSetMessage = (
+        <p className={style['warning-promotion']}>
+            <FontAwesomeIcon icon={faExclamationCircle} />
+            {reference.promotion}
+        </p>
+    );
+
+    let returnMessage = null
 
     if (reference.promotion) {
-      if (reference.promo === "1") {
-        if (qtde >= 2) {
-          renderMessage = (
-            <p className={style.warningPromotion}>
-              <FontAwesomeIcon icon={faExclamationCircle} />
-              {reference.promotion}
-            </p>
-          );
+        if (reference.promo === "1") {
+            if (qtde >= 2) returnMessage = toSetMessage
         }
-      }
-      if (reference.promo === "2") {
-        if (qtde >= 3) {
-          renderMessage = (
-            <p className={style.warningPromotion}>
-              <FontAwesomeIcon icon={faExclamationCircle} />
-              {reference.promotion}
-            </p>
-          );
+        if (reference.promo === "2") {
+            if (qtde >= 3) returnMessage = toSetMessage
         }
-      }
     }
 
-    return renderMessage;
-  };
+    return returnMessage
+};
 
 export default MessagePromotionProducts;
